ccc770d75763d810e09e9d35d592d566553367f7
Emre Kaplan <emre.kaplan@metglobal.com>
2018-08-15 15:14:50 +0300

    Added unit test

---
+++ src/test/java/com/metglobal/compass/logging/MockChain.java
@@ -0,0 +1,18 @@
+package com.metglobal.compass.logging;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * a mock implementation for testing because package has no services
+ */
+public class MockChain implements FilterChain {
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse)
+			throws IOException, ServletException {
+		servletResponse.getWriter().println("Here is your response");
+	}
+}
